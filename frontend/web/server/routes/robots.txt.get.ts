a50242f21bc31fb31f5b58a53ebe5ceef2e9bdca
Wineclouds04 <jqw5481@gmail.com>
2026-07-17 04:10:48 +0800

    Delete frontend directory

--- frontend/web/server/routes/robots.txt.get.ts
+++
@@ -1,6 +0,0 @@
-export default defineEventHandler((event) => {
-  const config = useRuntimeConfig(event)
-  const siteUrl = config.public.siteUrl.replace(/\/$/, '')
-  setHeader(event, 'content-type', 'text/plain; charset=utf-8')
-  return `User-agent: *\nAllow: /\nDisallow: /search\nSitemap: ${siteUrl}/sitemap.xml\n`
-})
--- frontend/web/server/utils/xml.ts
+++
@@ -1,7 +0,0 @@
-export const escapeXml = (value: unknown) =>
-  String(value ?? '')
-    .replaceAll('&', '&amp;')
-    .replaceAll('<', '&lt;')
-    .replaceAll('>', '&gt;')
-    .replaceAll('"', '&quot;')
-    .replaceAll("'", '&apos;')
